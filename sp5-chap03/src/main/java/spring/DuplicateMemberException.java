2f3070574810db4283aa46045b73517cbb11b660
Sang Jun Park <bobsang89@gmail.com>
2024-01-17 22:52:33 -0500

    commit

---
+++ sp5-chap03/src/main/java/spring/DuplicateMemberException.java
@@ -0,0 +1,13 @@
+package spring;
+
+/**
+ * Created by Sang Jun Park on 01/17/2024.
+ * GitHub : http://github.com/SangJun-GitHub
+ */
+
+public class DuplicateMemberException extends RuntimeException{
+
+    public DuplicateMemberException(String message){
+        super(message);
+    }
+}
---
+++ sp5-chap03/src/main/java/spring/RegisterRequest.java
@@ -0,0 +1,38 @@
+package spring;
+
+/**
+ * Created by Sang Jun Park on 01/17/2024.
+ * GitHub : http://github.com/SangJun-GitHub
+ */
+
+public class RegisterRequest {
+    private String email;
+    private String password;
+    private String confirmPassword;
+    private String name;
+
+    public String getEmail(){
+        return email;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public String getConfirmPassword(){
+        return confirmPassword;
+    }
+    public void setConfirmPassword(String confirmPassword){
+        this.confirmPassword = confirmPassword;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public boolean isPasswordEqualToConfirmPassword(){
+        return password.equals(confirmPassword);
+    }
+}

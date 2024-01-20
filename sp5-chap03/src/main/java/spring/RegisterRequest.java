703603a9feaeb37d8c4b1ec48dcc8bfa78b443dd
Sang Jun Park <bobsang89@gmail.com>
2024-01-19 22:43:39 -0500

    commit

--- sp5-chap03/src/main/java/spring/RegisterRequest.java
+++ sp5-chap03/src/main/java/spring/RegisterRequest.java
@@ -14,6 +14,9 @@ public class RegisterRequest {
     public String getEmail(){
         return email;
     }
+    public void setEmail(String email){
+        this.email = email;
+    }
     public String getPassword(){
         return password;
     }

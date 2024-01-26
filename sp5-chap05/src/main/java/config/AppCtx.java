b71c1e7bd481c0d757f36aca1480ccfd6d3f73e9
Sang Jun Park <bobsang89@gmail.com>
2024-01-25 19:51:15 -0500

    commit

--- sp5-chap05/src/main/java/config/AppCtx.java
+++ sp5-chap05/src/main/java/config/AppCtx.java
@@ -1,9 +1,39 @@
 package config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import spring.MemberPrinter;
+import spring.MemberSummaryPrinter;
+import spring.VersionPrinter;
+
 /**
  * Created by Sang Jun Park on 01/24/2024.
  * GitHub : http://github.com/SangJun-GitHub
  */
 
+@Configuration
+@ComponentScan(basePackages = {"spring"})
 public class AppCtx {
+
+    @Bean
+    @Qualifier("printer")
+    public MemberPrinter memberPrinter1(){
+        return new MemberPrinter();
+    }
+
+    @Bean
+    @Qualifier("summaryPrinter")
+    public MemberSummaryPrinter memberPrinter2(){
+        return new MemberSummaryPrinter();
+    }
+
+    @Bean
+    public VersionPrinter versionPrinter(){
+        VersionPrinter versionPrinter = new VersionPrinter();
+        versionPrinter.setMajorVersion(5);
+        versionPrinter.setMinorVersion(0);
+        return versionPrinter;
+    }
 }

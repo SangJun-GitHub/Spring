4061cf82e9ac6d051cf899c8abc903a5f365a325
Sang Jun Park <bobsang89@gmail.com>
2024-02-02 23:09:25 -0500

    commit

---
+++ sp5-chap08/src/main/java/spring/MemberDao.java
@@ -0,0 +1,26 @@
+package spring;
+
+import java.util.Collection;
+
+/**
+ * Created by Sang Jun Park on 02/02/2024.
+ * GitHub : http://github.com/SangJun-GitHub
+ */
+
+public class MemberDao {
+    public Member selectByEmail(String email){
+        return null;
+    }
+
+    public void insert(Member member){
+
+    }
+
+    public void update(Member member){
+
+    }
+
+    public Collection<Member> selectAll(){
+        return null;
+    }
+}

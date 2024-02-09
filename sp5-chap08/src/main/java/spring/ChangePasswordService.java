a928ea8ef369b7f03c07d2d634a51f9bd4566424
Sang Jun Park <bobsang89@gmail.com>
2024-02-08 23:24:44 -0500

    commit

--- sp5-chap08/src/main/java/spring/ChangePasswordService.java
+++ sp5-chap08/src/main/java/spring/ChangePasswordService.java
@@ -1,5 +1,7 @@
 package spring;
 
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * Created by Sang Jun Park on 02/03/2024.
  * GitHub : http://github.com/SangJun-GitHub
@@ -9,5 +11,18 @@ public class ChangePasswordService {
 
     private MemberDao memberDao;
 
+    @Transactional
+    public void changePassword(String email, String oldPwd, String newPwd){
+        Member member = memberDao.selectByEmail(email);
 
+        if(member == null)
+            throw new MemberNotFoundException();
+
+        member.changePassword(oldPwd, newPwd);
+        memberDao.update(member);
+    }
+
+    public void setMemberDao(MemberDao memberDao){
+        this.memberDao = memberDao;
+    }
 }
